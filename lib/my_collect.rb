e9e28697ff3daaba48bbe4f4edc629e783e154bf
Paul <pcherian10@gmail.com>
2018-04-30 23:34:19 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect (array)
+  i = 0
+  while (i < array.length)
+    yield (array[i])
+  i += 1
+  end
+  array
+end
+
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) {|lang| lang.upcase}
+
+
 
 
